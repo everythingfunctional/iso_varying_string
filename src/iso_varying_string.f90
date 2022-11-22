4c5bbf7188b8c6a7e2d45d44e05fb440bdb8aad7
Brad Richardson <everythingfunctional@protonmail.com>
2022-11-21 19:12:43 -0600

    refac: optimize <

--- src/iso_varying_string.f90
+++ src/iso_varying_string.f90
@@ -1195,31 +1195,51 @@ contains
         end if
     end function
 
-    elemental function string_lt_string(lhs, rhs) result(equals)
+    elemental function string_lt_string(lhs, rhs) result(less_than)
         ! Sec. 3.3.3
         type(varying_string), intent(in) :: lhs
         type(varying_string), intent(in) :: rhs
-        logical :: equals
+        logical :: less_than
 
-        equals = char(lhs) < char(rhs)
+        if (allocated(lhs%characters)) then
+            if (allocated(rhs%characters)) then
+                less_than = lhs%characters < rhs%characters
+            else
+                less_than = lhs%characters < ""
+            end if
+        else
+            if (allocated(rhs%characters)) then
+                less_than = "" < rhs%characters
+            else
+                less_than = .false.
+            end if
+        end if
     end function
 
-    elemental function character_lt_string(lhs, rhs) result(equals)
+    elemental function character_lt_string(lhs, rhs) result(less_than)
         ! Sec. 3.3.3
         character(len=*), intent(in) :: lhs
         type(varying_string), intent(in) :: rhs
-        logical :: equals
+        logical :: less_than
 
-        equals = lhs < char(rhs)
+        if (allocated(rhs%characters)) then
+            less_than = lhs < rhs%characters
+        else
+            less_than = lhs < ""
+        end if
     end function
 
-    elemental function string_lt_character(lhs, rhs) result(equals)
+    elemental function string_lt_character(lhs, rhs) result(less_than)
         ! Sec. 3.3.3
         type(varying_string), intent(in) :: lhs
         character(len=*), intent(in) :: rhs
-        logical :: equals
+        logical :: less_than
 
-        equals = char(lhs) < rhs
+        if (allocated(lhs%characters)) then
+            less_than = lhs%characters < rhs
+        else
+            less_than = "" < rhs
+        end if
     end function
 
     elemental function string_le_string(lhs, rhs) result(equals)
